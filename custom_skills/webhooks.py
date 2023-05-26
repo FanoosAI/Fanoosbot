97ae596375bfe0c1b54c8ba3ff746831decac85e
Saee <saee_saadat@outlook.com>
2023-05-26 19:30:56 +0330

    webhook

---
+++ custom_skills/webhooks.py
@@ -0,0 +1,29 @@
+import json
+import logging
+from aiohttp.web import Request, Response
+from opsdroid.skill import Skill
+from opsdroid.matchers import match_webhook
+
+
+class ButtonsSkill(Skill):
+    @match_webhook('buttons')
+    async def buttons(self, event: Request):
+        logging.info(f"Request for buttons -> {event}")
+        data = await event.json()
+        logging.info(f"Request data: {data}")
+        response_body = [
+            {
+                "name": "Hi",
+                "id": 1
+            },
+            {
+                "name": "Bye",
+                "id": 2
+            },
+            {
+                "name": "Help?",
+                "id": 3
+            },
+        ]
+        response_body = json.JSONEncoder().encode(response_body)
+        return Response(body=response_body, status=200)
